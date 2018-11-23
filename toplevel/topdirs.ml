9293ef7ecbf17d4d318784baea60db3d3c4275e2
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2018-11-23 16:23:15 +0100

    toplevel

--- toplevel/topdirs.ml
+++ toplevel/topdirs.ml
@@ -158,10 +158,8 @@ let rec load_file recursive ppf name =
   match filename with
   | None -> fprintf ppf "Cannot find file %s.@." name; false
   | Some filename ->
-      let ic = open_in_bin filename in
-      Misc.try_finally
-        ~always:(fun () -> close_in ic)
-        (fun () -> really_load_file recursive ppf name filename ic)
+      Misc.with_in_bin filename @@ fun ic ->
+      really_load_file recursive ppf name filename ic
 
 and really_load_file recursive ppf name filename ic =
   let buffer = really_input_string ic (String.length Config.cmo_magic_number) in
