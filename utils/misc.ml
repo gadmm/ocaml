ace6fb68502699843bf303b4b931bf4bc491a256
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2018-11-23 18:19:26 +0100

    Remove try_finally

--- utils/misc.ml
+++ utils/misc.ml
@@ -27,28 +27,6 @@ let fatal_error msg = fatal_errorf "%s" msg
 
 (* Exceptions *)
 
-let try_finally ?(always=(fun () -> ())) ?(exceptionally=(fun () -> ())) work =
-  match work () with
-    | result ->
-      begin match always () with
-        | () -> result
-        | exception always_exn ->
-          let always_bt = Printexc.get_raw_backtrace () in
-          exceptionally ();
-          Printexc.raise_with_backtrace always_exn always_bt
-      end
-    | exception work_exn ->
-      let work_bt = Printexc.get_raw_backtrace () in
-      begin match always () with
-        | () ->
-          exceptionally ();
-          Printexc.raise_with_backtrace work_exn work_bt
-        | exception always_exn ->
-          let always_bt = Printexc.get_raw_backtrace () in
-          exceptionally ();
-          Printexc.raise_with_backtrace always_exn always_bt
-      end
-
 let try_and_reraise ~exceptionally f =
   try f () with e -> exceptionally (); raise e
 
