b22b63a7db1f6cd3defc351c1c14ce2a1d8b70ec
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2021-07-19 23:47:26 +0200

    Fix merge

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -630,9 +630,10 @@ Caml_noinline static intnat do_some_marking
   struct mark_stack stk = *Caml_state->mark_stack;
 
 #ifdef NO_NAKED_POINTERS
-  uintnat young_start = (uintnat)Val_hp(Caml_state->young_start);
+  uintnat young_start = (uintnat)Val_hp(Caml_state->young_alloc_start);
   uintnat half_young_len =
-    ((uintnat)Caml_state->young_end - (uintnat)Caml_state->young_start) >> 1;
+    ((uintnat)Caml_state->young_alloc_end
+     - (uintnat)Caml_state->young_alloc_start) >> 1;
 #define Is_block_and_not_young(v) \
   (((intnat)rotate1((uintnat)v - young_start)) > (intnat)half_young_len)
 #endif
