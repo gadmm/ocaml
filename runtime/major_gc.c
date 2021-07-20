bf53cc1cb5cd1b8c61660eb35a0ed53cc471f2b3
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2021-07-21 00:45:19 +0200

    Fix compilation

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -633,7 +633,7 @@ Caml_noinline static intnat do_some_marking
 #ifdef NO_NAKED_POINTERS
   uintnat young_start = (uintnat)Val_hp(Caml_state->young_alloc_start);
   uintnat half_young_len =
-    ((uintnat)Caml_state->young_alloc_end - young_alloc_start) >> 1;
+    ((uintnat)Caml_state->young_alloc_end - young_start) >> 1;
 #define Is_block_and_not_young(v) \
   (((intnat)rotate1((uintnat)v - young_start)) >= (intnat)half_young_len)
 #endif
