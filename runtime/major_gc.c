c3fd2083dd89d38a6f237ec2286e634f5659bb4d
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2021-12-05 16:42:09 +0100

    Reimplement forward optimization

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -632,19 +632,35 @@ Caml_noinline static intnat do_some_marking
 #define Is_not_young(v) (((uintnat)v - young_start) >= young_len)
 #define Is_markable(v) Is_not_young(v)
 #endif
+  value * pb_orig[Pb_size];
 
   while (1) {
     value *scan, *obj_end, *scan_end;
 
     if (pb_enqueued > pb_dequeued + min_pb) {
       /* Dequeue from prefetch buffer */
-      value block = pb[(pb_dequeued++) & Pb_mask];
+      value block = pb[pb_dequeued & Pb_mask];
       header_t hd = Hd_val(block);
 
-      if (Tag_hd(hd) == Infix_tag) {
+      tag_t tag = Tag_hd(hd);
+      if (tag == Infix_tag) {
         block -= Infix_offset_val(block);
         hd = Hd_val(block);
+      } else if (UNLIKELY(tag == Forward_tag)) {
+        value f = Forward_val(block);
+        if (!Is_block(f) || (Is_in_value_area(f)
+                             && Tag_val (f) != Forward_tag
+                             && Tag_val (f) != Lazy_tag
+                             && Tag_val (f) != Double_tag)) {
+          /* Short-circuit the pointer */
+          /* [block] is not changed because it must be marked alive */
+          value *orig = pb_orig[pb_dequeued & Pb_mask];
+          *orig = f;
+          if (Is_block(f) && Is_young(f) && !Is_young(block))
+            add_to_ref_table (Caml_state->ref_table, orig);
+        }
       }
+      pb_dequeued++;
 
 #ifdef NO_NAKED_POINTERS
       /* See [caml_darken] for a description of this assertion. */
@@ -703,7 +719,9 @@ Caml_noinline static intnat do_some_marking
           break; /* Prefetch buffer is full */
         }
         prefetch_block(v);
-        pb[(pb_enqueued++) & Pb_mask] = v;
+        pb[pb_enqueued & Pb_mask] = v;
+        pb_orig[pb_enqueued & Pb_mask] = scan;
+        pb_enqueued++;
       }
     }
 
