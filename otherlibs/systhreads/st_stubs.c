af2f604080279c240ac0495cb0efc63f09ab98d1
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2022-07-05 21:43:24 +0200

    [minor] Some documentation and FIXMEs for Unix.fork

--- otherlibs/systhreads/st_stubs.c
+++ otherlibs/systhreads/st_stubs.c
@@ -364,6 +364,10 @@ static void caml_thread_remove_and_free(caml_thread_t th)
 
 static void caml_thread_reinitialize(void)
 {
+  /* Note: "If a multi-threaded process calls fork() [...] the child
+     process may only execute async-signal-safe operations until such
+     time as one of the exec functions is called." (POSIX fork()) */
+
   caml_thread_t th, next;
 
   th = Active_thread->next;
@@ -383,8 +387,14 @@ static void caml_thread_reinitialize(void)
      st_masterlock_acquire (busy = 1) */
   st_masterlock *m = Thread_lock(Caml_state->id);
   m->init = 0; /* force reinitialization */
+  /* Note: initializing an already-initialized mutex and cond variable
+     is UB (especially mutexes that are locked). This is best
+     effort. */
   if (0 != st_masterlock_init(m))
     caml_fatal_error("Unix.fork: failed to reinitialize master lock");
+  /* FIXME: The same should be done (or not) for IO mutexes (as in the
+     pre-multicore world). However there they might be locked by
+     someone else, and we are probably in an inconsistent state. */
 }
 
 CAMLprim value caml_thread_join(value th);
