5b94a0c35d3123f416d94bf01dfc29915a8737bc
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@inria.fr>
2022-09-10 05:37:36 +0200

    Update stats log file

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -847,11 +847,12 @@ static void mark_slice (intnat work)
     int err = 0;
     if (NULL == out_immediates_stats) {
 #ifdef NO_NAKED_POINTERS
-#define SUFFIX "-nnp.log"
+#define SUFFIX "+nnp.log"
 #else
-#define SUFFIX "-pt.log"
+#define SUFFIX ".log"
 #endif
-      char * out_file_name = "/tmp/ocaml-stats-mark-prefetching" SUFFIX;
+      char * out_file_name =
+        "/tmp/ocaml-stats-mark-prefetching-412-stats" SUFFIX;
       if (NULL == out_file_name) goto out;
       out_immediates_stats = fopen(out_file_name, "a");
       if (NULL == out_immediates_stats) goto out;
